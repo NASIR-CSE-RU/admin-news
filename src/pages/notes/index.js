ab47f64e4789c19eb5f652b90e4624c5b831577c
NASIR-CSE-RU <nasir.cse.ru@gmail.com>
2023-03-23 01:27:38 +0600

    add new line before return

--- src/pages/notes/index.js
+++ src/pages/notes/index.js
@@ -2,11 +2,11 @@
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
-import { useEffect,useState } from 'react'
 import NotesTable from 'src/@core/components/notes/NotesTable'
 import fetchAPI from 'src/lib/fetchApi'
 
-const MUITable = (props) => {        
+const MUITable = (props) => {
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
@@ -22,9 +22,11 @@ const MUITable = (props) => {
 MUITable.getInitialProps = async (ctx) => {
   try {
     const res = await fetchAPI('/notes', { method: 'GET' });
+
     return { notes: res.data };
   } catch (error) {
     console.log(error);
+    
     return { notes: [] }; // fallback value in case of error
   }
 };
